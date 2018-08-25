8c56616837cc09ac35138e967bb08ccbd131e27d
Jonathan IMG <join_mg@hotmail.com>
2018-08-24 23:10:05 -0500

    Try catch
    Try catch Test example

--- js/38-try-catch.js
+++ js/38-try-catch.js
@@ -2,4 +2,18 @@
 
 window.addEventListener('load', function(){
 
+    try{
+        /*Error para testear try catch 
+        var year = 2018;
+        alert(yea);*/
+        //decodeURIComponent nos ayuda a ver si una URL es valida ya que no tenga caracteres invalidos
+        console.log(decodeURIComponent('https://victorroblesweb.es'));
+        //URL Invalida para ver el error
+        //console.log(decodeURIComponent('https://v#$%ictorroblesweb.es'));
+    }catch(error){
+        console.log("A ocurrido un error en el codigo")
+    }
+    
+    
+
 });
